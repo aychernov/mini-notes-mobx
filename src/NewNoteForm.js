35c7ef774564f24094ebcdbcbeaa6bd654ebd647
tallboy <yourspaceworkteam@gmail.com>
2022-05-12 19:43:43 +0300

    init project

---
+++ src/NewNoteForm.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import {useNotesStore} from "./notesContext";
+
+const NewNoteForm = () => {
+
+    const [noteText, setNoteText] = useState('')
+    const notesStore = useNotesStore()
+
+    return (
+        <>
+            <input
+                value={noteText}
+                onChange={(e) => setNoteText(e.target.value)}
+                type="text"/>
+
+            <button onClick={ () => notesStore.addNote(noteText)}>Add note</button>
+        </>
+    );
+};
+
+export default NewNoteForm;
